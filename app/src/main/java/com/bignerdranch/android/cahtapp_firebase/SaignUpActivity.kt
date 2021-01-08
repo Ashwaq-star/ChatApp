9fd108e8092d74c61d23526bd50ee1b9ea5a4655
Ashwaq Alsaedi <ashwaq.alsaedituwaiq.edu.sals -al ~/.ssh$ ssh-keygen -t rsa -b 4096 -c ashwaq.alsaedituwaiq.edu.sa>
2021-01-08 17:56:38 +0300

    establishing firebase to the android app

--- app/src/main/java/com/bignerdranch/android/cahtapp_firebase/SaignUpActivity.kt
+++ app/src/main/java/com/bignerdranch/android/cahtapp_firebase/SaignUpActivity.kt
@@ -3,9 +3,9 @@ package com.bignerdranch.android.cahtapp_firebase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SaignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
     }
 }
